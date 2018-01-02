5d0e1e0f5199d656f5e376c1417a5c1fcd2401de
zero <xue.zl@qq.com>
2018-01-02 22:55:02 +0800

    v39 TreeDepth and IsBalanceTree

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <unordered_map>
 #include <vector>
-#include <string>
 using std::cout;
 using std::endl;
-using std::string;
-using std::unordered_map;
 using std::vector;
 
 /**
@@ -15,25 +11,58 @@ using std::vector;
  * （2）平衡二叉树：即任意结点的左右子树深度相差不超过1
  */
 
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 class Solution
 {
   public:
-    int FirstNotRepeatingChar(string str)
+    int TreeDepth(TreeNode *pRoot)
     {
-        if (str.empty())
-            return -1;
-        unordered_map<char, int> mapCount;
-        for (const auto &c : str)
-            ++mapCount[c];
-        for (size_t i = 0; i < str.size(); ++i)
-            if (mapCount[str[i]] == 1)
-                return int(i);
-        return -1; //没有这样的字符返回-1
+        if (!pRoot)
+            return 0;
+        int nLeft = TreeDepth(pRoot->left);
+        int nRight = TreeDepth(pRoot->right);
+        return nLeft > nRight ? nLeft + 1 : nRight + 1;
+    }
+    bool IsBalanced_Solution(TreeNode *pRoot)
+    {
+        int depth = 0;
+        return IsBalancedRecursion(pRoot, depth);
+    }
+
+  private:
+    /**
+     * 思路
+     * 递归到叶结点，从下往上开始比较,需要有个depth一直保留深度
+     */
+    bool IsBalancedRecursion(TreeNode *pRoot, int &depth)
+    {
+        if (!pRoot)
+        {
+            depth = 0;
+            return true;
+        }
+        int nLeft, nRight; //声明为局部变量，即让父节点获取子节点的深度即可
+        if (IsBalancedRecursion(pRoot->left, nLeft) &&
+            IsBalancedRecursion(pRoot->right, nRight))
+        {
+            int nDiff = nLeft > nRight ? nLeft - nRight : nRight - nLeft;
+            if (nDiff <= 1)
+            {
+                depth = nLeft > nRight ? nLeft + 1 : nRight + 1;
+                return true;
+            }
+        }
+        return false;
     }
 };
 
 int main()
 {
-    Solution s;
-    cout << s.FirstNotRepeatingChar("adebeadc") << endl;
+    // Solution s; //已通过牛客网测试用例
 }
--- subject/Exam_39_TreeDepth_Balance.cpp
+++ subject/Exam_39_TreeDepth_Balance.cpp
@@ -31,14 +31,34 @@ class Solution
     }
     bool IsBalanced_Solution(TreeNode *pRoot)
     {
+        int depth = 0;
+        return IsBalancedRecursion(pRoot, depth);
+    }
+
+  private:
+    /**
+     * 思路
+     * 递归到叶结点，从下往上开始比较,需要有个depth一直保留深度
+     */
+    bool IsBalancedRecursion(TreeNode *pRoot, int &depth)
+    {
         if (!pRoot)
+        {
+            depth = 0;
             return true;
-        int nLeft = TreeDepth(pRoot->left);
-        int nRight = TreeDepth(pRoot->right);
-        int nDiff = nLeft > nRight ? nLeft - nRight : nRight - nLeft;
-        if (nDiff > 1)
-            return false;
-        return IsBalanced_Solution(pRoot->left) && IsBalanced_Solution(pRoot->right);
+        }
+        int nLeft, nRight; //声明为局部变量，即让父节点获取子节点的深度即可
+        if (IsBalancedRecursion(pRoot->left, nLeft) &&
+            IsBalancedRecursion(pRoot->right, nRight))
+        {
+            int nDiff = nLeft > nRight ? nLeft - nRight : nRight - nLeft;
+            if (nDiff <= 1)
+            {
+                depth = nLeft > nRight ? nLeft + 1 : nRight + 1;
+                return true;
+            }
+        }
+        return false;
     }
 };
 
